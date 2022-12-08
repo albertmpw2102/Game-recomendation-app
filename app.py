9b3773d7e0936f96bc6888a854a41d2f4c28771e
albertmpw2102 <99249999+albertmpw2102@users.noreply.github.com>
2022-12-08 15:44:34 +0000

    questionnaire page added

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-fromfrom flask import Flask, render_template, request, session, url_for, redirect
+from flask import Flask, render_template, request, session, url_for, redirect
 app = Flask(__name__)
 import sqlite3
 
 app.secret_key = 'LOL_SECRET_KEY'
+
+#NOTES
+#IF NOT MUCH TIME LEFT AND USING API IS TOO COMPLICATED, INSERT 7-10 GAMES YOURSELF AND WORK WITH IT
+
 
 #PAGES
 
@@ -23,24 +27,24 @@ def signupPage():
 @app.route('/signup', methods=['GET','POST'])
 def signup():
 
+        # creates an account for the user, the validation is done client side for now#
+        # if it is successful, it redirects the user to 'questionnaire' page 
+
         con = sqlite3.connect('userdata.db')
         cur = con.cursor()
         cur.execute("""INSERT INTO user (username, name, email, password)
                         VALUES (?,?,?,?)""",
                         (request.form['usern'],request.form['name'],request.form['mail'],request.form['pass']))
         con.commit()
-        cur.execute("""
-                INSERT INTO settings (username, theme, fontsize)
-                VALUES (?,?,?)""", (request.form['usern'], '1', '25')
-
-                )
+        cur.execute("""INSERT INTO settings (username, theme, fontsize)
+                        VALUES (?,?,?)""",
+                        (request.form['usern'], '1', '25'))
         con.commit()
         con.close()
 
         session['user'] = request.form['usern']
 
-        return redirect(url_for('home'))
-
+        return redirect(url_for('interest_test'))
 
 
 
@@ -50,11 +54,14 @@ def login():
     cur = con.cursor()
     cur.execute("""SELECT * FROM user WHERE username=? AND password=?""",
                     (request.form['usern'],request.form['pass']))
+
     match = len(cur.fetchall())
+
     if match == 0:
         return "Wrong username or password"
     else:
         session['user'] = request.form['usern']
+
         return redirect(url_for('home'))
 
 
@@ -64,6 +71,7 @@ def login():
 def home():
     con = sqlite3.connect('userdata.db')
     cur = con.cursor()
+
     if session.get('user') == None:
         return render_template('home.html', name="Welcome, idiot")
     else:
@@ -91,16 +99,16 @@ def tech_support():
             return render_template('tech-support.html', match=1)
 
     elif request.method == 'POST':
-        cur.execute("""
-
-        INSERT INTO messages (username, reason, text)
-        VALUES (?,?,?)""",
-        (session['user'], request.form['reason'], request.form['text']))
+        cur.execute("""INSERT INTO messages (username, reason, text)
+                        VALUES (?,?,?)""",
+                        (session['user'], request.form['reason'], request.form['text']))
         con.commit()
         con.close()
-        return render_template('tech-support.html', message="thank you for the message")
+
+        return render_template('tech-support.html', match=1)
 
     else:
+
         return render_template('tech-support.html')
 
 
@@ -118,9 +126,9 @@ def settings():
     con = sqlite3.connect('userdata.db')
     cur = con.cursor()
     cur.execute("""SELECT * FROM settings WHERE username=?""",
-    (session['user'],)
-    )
+                (session['user'],))
     set = cur.fetchall()
+
     return render_template('settings.html', fontsize=str(set[0][2]), theme=str(set[0][1]))
 
 
@@ -134,6 +142,7 @@ def saveTheme():
                     WHERE username=?""", (theme, session['user']))
     con.commit()
     con.close()
+
     return redirect(url_for('settings'))
     #maybe would be more effiecient without the get method, for example ajax
 
@@ -146,6 +155,7 @@ def saveOtherSettings():
                     WHERE username=?""", (request.form['fontSize'], session['user']))
     con.commit()
     con.close()
+
     return redirect(url_for('settings'))
 
 
@@ -163,309 +173,35 @@ def board():
     set = cur.fetchall()
     return render_template('dev_board_messages.html', messages=set)
 
-
-
-#CREATING TABLES
-@app.route('/create')
-def create():
+@app.route('/interest_test')
+def interest_test():
     con = sqlite3.connect('userdata.db')
     cur = con.cursor()
-    cur.execute("""
+    cur.execute("""SELECT * FROM genres""")
+    genres = cur.fetchall()
 
-        CREATE TABLE user (
-        username VARCHAR(15) NOT NULL PRIMARY KEY,
-        name VARCHAR(12),
-        email VARCHAR(30),
-        password VARCHAR(20)
-        )
-        """)
-    con.commit()
-    con.close()
-    return 'tbl created!'
+    for x in range(len(genres)):
+        genres[x] = genres[x][0]
+
+    return render_template('interest_test.html', genres=genres)
 
 
 
+#INSERTING FUNCTIONS
 
-@app.route('/message-table-create')
-def message_table_create():
-    con = sqlite3.connect('userdata.db')
-    cur = con.cursor()
-    cur.execute("""
-        CREATE TABLE messages (
-        username VARCHAR(15) NOT NULL PRIMARY KEY,
-        reason VARCHAR(20),
-        text VARCHAR(500)
-        )
-        """)
-    con.commit()
-    con.close()
-    return 'message tbl created!'
-
-
-
-
-@app.route('/settings-table-create')
-def settings_table_create():
-    con = sqlite3.connect('userdata.db')
-    cur = con.cursor()
-    cur.execute("""
-        CREATE TABLE settings
-        (
-        username VARCHAR(15) NOT NULL PRIMARY KEY,
-        theme INTEGER NOT NULL,
-        fontsize INTEGER
-        )
-        """)
-    con.commit()
-    con.close()
-    return 'settings tbl created!'
-
-
-
-#SEE FUNCTIONS
-@app.route('/see')
-def see():
-    con = sqlite3.connect('userdata.db')
-    cur = con.cursor()
-    cur.execute("""
-                SELECT * FROM user;
-        """)
-    rows = cur.fetchall()
-    return str(rows)
-
-
-
-
-@app.route('/see-messages')
-def see_messages():
-    con = sqlite3.connect('userdata.db')
-    cur = con.cursor()
-    cur.execute("""
-                SELECT * FROM messages;
-        """)
-    rows = cur.fetchall()
-    return str(rows)
-
-
-
-
-@app.route('/see-settings')
-def see_settings():
-    con = sqlite3.connect('userdata.db')
-    cur = con.cursor()
-    cur.execute("""
-                SELECT * FROM settings;
-        """)
-    rows = cur.fetchall()
-    return str(rows)
-
-
-
-#CLEARING FUNCTIONS
-@app.route('/clear-messages')
-def clear_messages():
-    con = sqlite3.connect('userdata.db')
-    cur = con.cursor()
-    cur.execute("""
-                DELETE FROM messages;
-        """)
-    con.commit()
-    con.close()
-    return 'messages cleared'
-
-
-
-
-@app.route('/clear-usertable')
-def clear_message():
-    con = sqlite3.connect('userdata.db')
-    cur = con.cursor()
-    cur.execute("""
-                DELETE FROM user;
-        """)
-    con.commit()
-    con.close()
-    return 'users cleared'
-
-
-@app.route('/clear-sessions')
-def clear_sessions():
-    session.clear()
-
-    return "cleared"
-
-@app.route('/clear-all')
-def clear_all():
-    session.clear()
-    con = sqlite3.connect('userdata.db')
-    cur = con.cursor()
-    cur.execute("""
-                DELETE FROM user;
-        """)
-    con.commit()
-    cur.execute("""
-                DELETE FROM messages;
-        """)
-    con.commit()
-    cur.execute("""
-                DELETE FROM settings;
-        """)
-    con.commit()
-    con.close()
-    return "all cleared"
- flask import Flask, render_template, request, session, url_for, redirect
-app = Flask(__name__)
-import sqlite3
-
-app.secret_key = 'LOL_SECRET_KEY'
-
-#PAGES
-
-@app.route('/')
-def login_page():
-        return render_template('login.html')
-
-
-
-
-@app.route('/creating_account')
-def signupPage():
-        return render_template('creating_account.html')
-
-
-
-
-@app.route('/signup', methods=['GET','POST'])
-def signup():
-
+@app.route('/insert-genres')
+def insert_genres():
+    genres = ['Horror', 'PsychologicalHorror', 'GoodAtmosphere', 'Survival', 'FPS', 'Fighting', 'SwordFighting', 'SoulsLike', 'Simulator', 'PvP', 'PvE', 'SinglePlayer', 'Coop', 'BattleRoyal',
+    'Sports', 'Puzzle', 'FreeToPlay', 'Racing', 'Action', 'Adventure', 'Apocalyptic']
+    for x in range(len(genres)):
         con = sqlite3.connect('userdata.db')
         cur = con.cursor()
-        cur.execute("""INSERT INTO user (username, name, email, password)
-                        VALUES (?,?,?,?)""",
-                        (request.form['usern'],request.form['name'],request.form['mail'],request.form['pass']))
-        con.commit()
-        cur.execute("""
-                INSERT INTO settings (username, theme, fontsize)
-                VALUES (?,?,?)""", (request.form['usern'], '1', '25')
-
-                )
+        cur.execute("""INSERT INTO genres (genre)
+                        VALUES (?)""", (genres[x],))
         con.commit()
         con.close()
+    return 'all genres are inserted!'
 
-        session['user'] = request.form['usern']
-
-        return redirect(url_for('home'))
-
-
-
-
-@app.route('/login', methods=['POST'])
-def login():
-    con = sqlite3.connect('userdata.db')
-    cur = con.cursor()
-    cur.execute("""SELECT * FROM user WHERE username=? AND password=?""",
-                    (request.form['usern'],request.form['pass']))
-    match = len(cur.fetchall())
-    if match == 0:
-        return "Wrong username or password"
-    else:
-        session['user'] = request.form['usern']
-        return redirect(url_for('home'))
-
-
-
-
-@app.route('/home')
-def home():
-    con = sqlite3.connect('userdata.db')
-    cur = con.cursor()
-    if session.get('user') == None:
-        return render_template('home.html', name="Welcome, guest")
-    else:
-        cur.execute("""SELECT name FROM user WHERE username=?""",
-                        (session['user'],))
-        name = cur.fetchall()
-        if session['user'] == 'albert_dev':
-            return render_template('home.html', tool="<a href=dev_board id=board>Dev board</a>",
-                                                name="Welcome, " + name[0][0])
-        else:
-            return render_template('home.html', name="Welcome, " + name[0][0])
-
-
-
-
-@app.route('/tech-support', methods=['GET', 'POST'])
-def tech_support():
-    con = sqlite3.connect('userdata.db')
-    cur = con.cursor()
-    cur.execute("""
-        SELECT * FROM messages WHERE username=?""",
-        (session['user'],))
-    match = len(cur.fetchall())
-    if match != 0:
-            return render_template('tech-support.html', match=1)
-
-    elif request.method == 'POST':
-        cur.execute("""
-
-        INSERT INTO messages (username, reason, text)
-        VALUES (?,?,?)""",
-        (session['user'], request.form['reason'], request.form['text']))
-        con.commit()
-        con.close()
-        return render_template('tech-support.html', message="thank you for the message")
-
-    else:
-        return render_template('tech-support.html')
-
-
-
-
-@app.route('/catalogue')
-def catalogue():
-    return render_template('catalogue.html')
-
-
-
-
-@app.route('/settings', methods=['POST','GET'])
-def settings():
-    con = sqlite3.connect('userdata.db')
-    cur = con.cursor()
-    cur.execute("""SELECT * FROM settings WHERE username=?""",
-    (session['user'],)
-    )
-    set = cur.fetchall()
-    return render_template('settings.html', fontsize=str(set[0][2]), theme=str(set[0][1]))
-
-
-@app.route('/settings/savetheme', methods=['GET'])
-def saveTheme():
-    con = sqlite3.connect('userdata.db')
-    cur = con.cursor()
-    theme = request.args.get('theme', '')
-    cur.execute("""UPDATE settings
-                    SET theme=?
-                    WHERE username=?""", (theme, session['user']))
-    con.commit()
-    con.close()
-    return redirect(url_for('settings'))
-    #maybe would be more effiecient without the get method, for example ajax
-
-
-
-@app.route('/wishlist')
-def wishlist():
-    return render_template('wishlist.html')
-
-
-@app.route('/dev_board')
-def board():
-    con = sqlite3.connect('userdata.db')
-    cur = con.cursor()
-    cur.execute("""SELECT * FROM messages""")
-    set = cur.fetchall()
-    return render_template('dev_board_messages.html', messages=set)
 
 
 
@@ -525,6 +261,21 @@ def settings_table_create():
     return 'settings tbl created!'
 
 
+@app.route('/create-table-genres')
+def create_table_genres():
+    con = sqlite3.connect('userdata.db')
+    cur = con.cursor()
+    cur.execute("""
+        CREATE TABLE genres
+        (
+        genre VARCHAR(20) NOT NULL PRIMARY KEY
+        )
+        """)
+    con.commit()
+    con.close()
+    return 'genres table is created!'
+
+
 
 #SEE FUNCTIONS
 @app.route('/see')
@@ -559,6 +310,16 @@ def see_settings():
     cur = con.cursor()
     cur.execute("""
                 SELECT * FROM settings;
+        """)
+    rows = cur.fetchall()
+    return str(rows)
+
+@app.route('/see-genres')
+def see_genres():
+    con = sqlite3.connect('userdata.db')
+    cur = con.cursor()
+    cur.execute("""
+                SELECT * FROM genres;
         """)
     rows = cur.fetchall()
     return str(rows)
@@ -617,3 +378,6 @@ def clear_all():
     con.commit()
     con.close()
     return "all cleared"
+
+
+
